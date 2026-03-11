5e93c9c5accc06e6be76d99af4771f86c394f485
Bruce W. Herr II <bherr@iu.edu>
2026-03-11 10:24:18 -0400

    Add terminology query and some optimization

---
+++ queries/reports/validation/wpp-asctb-terminology-summary.sql
@@ -0,0 +1,29 @@
+.mode csv
+SELECT *
+FROM (
+  SELECT '# AS only in WPP' as label, COUNT(*) as count
+  FROM read_csv(getenv('OUTPUT_DIR') || '/reports/validation/wpp-uberon-missing-in-asctb.csv')
+)
+UNION ALL (
+  SELECT '# AS only in HRA' as label, COUNT(*) as count 
+  FROM read_csv(getenv('OUTPUT_DIR') || '/reports/validation/wpp-uberon-only-in-asctb.csv')
+)
+UNION ALL (
+  SELECT '# AS in WPP & HRA' as label, COUNT(*) as count 
+  FROM read_csv(getenv('OUTPUT_DIR') || '/reports/validation/wpp-uberon-present-in-asctb.csv')
+)
+UNION ALL (
+  SELECT '# CT only in HRA' as label, COUNT(*) as count
+  FROM read_csv(getenv('OUTPUT_DIR') || '/reports/validation/wpp-cl-only-in-asctb.csv')
+)
+UNION ALL (
+  SELECT '# CT only in WPP' as label, COUNT(*) as count
+  FROM read_csv(getenv('OUTPUT_DIR') || '/reports/validation/wpp-cl-missing-in-asctb.csv')
+)
+UNION ALL (
+  SELECT '# CT in WPP & HRA' as label, COUNT(*) as count
+  FROM read_csv(getenv('OUTPUT_DIR') || '/reports/validation/wpp-cl-present-in-asctb.csv')
+)
+UNION ALL (
+  SELECT * FROM read_csv('/dev/stdin')
+)
--- scripts/75-run-reports.sh
+++ scripts/75-run-reports.sh
@@ -6,4 +6,8 @@ set -ev
 DIR=$OUTPUT_DIR
 FILTER="$1"
 
+if [ "$FILTER" = "" ]; then
+  # These need to be run first on a full run
+  node ./src/run-reports-blazegraph.js $BLAZEGRAPH_DB $DIR/reports -in-asctb
+fi
 node ./src/run-reports-blazegraph.js $BLAZEGRAPH_DB $DIR/reports $FILTER
