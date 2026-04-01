e96c78e36dba4546b756be739bad746d7cd81d5c
Bruce W. Herr II <bherr@iu.edu>
2026-04-01 09:41:20 -0400

    Make the create-invalidation not spawn less for whatever reason

--- jobs/deploy.sh
+++ jobs/deploy.sh
@@ -9,6 +9,6 @@ if [ "${CDN_S3_BUCKET}" != "" ]; then
   aws s3 sync --profile wpp --exclude "${OUTPUT_DIR}/.git/*" $CLEAN ${OUTPUT_DIR}/ ${CDN_S3_BUCKET}
 
   if [ "${CDN_DISTRIBUTION_ID}" != "" ]; then
-    aws cloudfront --profile wpp create-invalidation --distribution-id ${CDN_DISTRIBUTION_ID} --paths "/data-products/*"
+    aws cloudfront --profile wpp create-invalidation --distribution-id ${CDN_DISTRIBUTION_ID} --paths "/data-products/*" > /dev/null
   fi
 fi
